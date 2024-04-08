629941e114695d6edd0538adf2879a4c0c8ba29c
Eyal Cherevatsky <eyal.ch@icloud.com>
2024-04-08 21:08:15 +0300

    feat: add endpoint for getting a CSV of scan results

--- scanner/main.py
+++ scanner/main.py
@@ -1,6 +1,7 @@
 import uuid
 
 from fastapi import Depends, FastAPI, HTTPException
+from fastapi.responses import StreamingResponse
 
 from . import schemas, service, tasks
 from .database import Session
@@ -44,3 +45,17 @@ async def get_scan(scan_id: uuid.UUID, db: Session = Depends(get_db)):
         raise HTTPException(status_code=404, detail="Scan not found")
 
     return scan
+
+
+@app.get("/scans/{scan_id}/results")
+async def get_scan_results(scan_id: uuid.UUID, db: Session = Depends(get_db)):
+    scan = service.get_scan(db, scan_id)
+
+    if scan is None:
+        raise HTTPException(status_code=404, detail="Scan not found")
+
+    return StreamingResponse(
+        service.generate_results_csv(scan.results),
+        media_type="text/csv",
+        headers={"Content-Disposition": f'attachment; filename="{scan_id}.csv"'},
+    )
--- scanner/service.py
+++ scanner/service.py
@@ -1,9 +1,10 @@
+import csv
 import uuid
 
 from sqlalchemy import select
 from sqlalchemy.orm import Session
 
-from .models import Scan
+from .models import Result, Scan
 
 
 def check_in_progress_scan(db: Session, domain: str) -> bool:
@@ -27,3 +28,24 @@ def get_scans(db: Session):
 def get_scan(db: Session, scan_id: uuid.UUID):
     stmt = select(Scan).filter_by(id=scan_id)
     return db.execute(stmt).scalar_one_or_none()
+
+
+class DummyWriter:
+    @staticmethod
+    def write(line):
+        return line
+
+
+def generate_results_csv(results: list[Result]):
+    writer = csv.DictWriter(DummyWriter(), fieldnames=["tool", "type", "value"])
+
+    yield writer.writeheader()
+
+    for result in results:
+        yield writer.writerow(
+            {
+                "tool": result.tool,
+                "type": result.type,
+                "value": result.value,
+            }
+        )
